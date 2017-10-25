fee9f5a6525671e767ac8a0625f3f1d56d23d281
Hamza Tahboub <hamza.tahboub8@gmail.com>
2017-10-25 16:01:20 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-#Import all required libraries and modules (this list will grow with time)
-import sys
-import cv2
-import numpy
-import matplotlib
-import Tkinter
-import win32api, win32com
-import time
-import math
